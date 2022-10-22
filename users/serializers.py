010873c97877878fa4593dc260caef468a927c51
unknown <kheyhan31@gmail.com>
2022-10-22 20:24:22 +0800

    change create to save in users serializers

--- users/serializers.py
+++ users/serializers.py
@@ -22,7 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = User.objects.save(
             email=validated_data['email'],
             password=validated_data['password']
         )
